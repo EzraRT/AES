3b67ad62fd9d9dbbd24576a53342201d1a2583e1
EzraRT <platoongo@gmail.com>
2020-12-02 18:41:57 +0800

    添加测试样例

--- test/test.cc
+++ test/test.cc
@@ -1,18 +1,21 @@
+#define BOOST_TEST_MODULE AES_TESTS
+
 #include "decryption.h"
 #include "encryption.h"
-#include <string>
-#include <fstream>
-#include <iostream>
 #include <cstddef>
 #include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
 
-#define BOOST_TEST_MODULE AESTests
-
+#include <boost/shared_ptr.hpp>
 #include <boost/test/unit_test.hpp>
 
+using namespace boost;
 using namespace std;
 
-string rand_str(size_t length) {
+string rand_str(size_t length)
+{
     string result;
     for (size_t i = 0; i < length; ++i) {
         result.append(1, char(32 + rand() % 95));
@@ -20,12 +23,13 @@ string rand_str(size_t length) {
     return result;
 }
 
-BOOST_AUTO_TEST_CASE(PassTest) {
+BOOST_AUTO_TEST_CASE(First_Test_Case)
+{
     srand(time(NULL));
-    std::string teststr = rand_str(rand() % 10000),
-            password("12345678"),
-            rawfilename("raw.txt"),
-            filename("temp.bin");
+    std::string teststr = rand_str(rand() % 10000 + 1000ull),
+                password("12345678"),
+                rawfilename("raw.txt"),
+                filename("temp.bin");
 
     std::fstream rawfile(rawfilename, std::ios::binary | std::ios::out);
     rawfile << teststr;
@@ -36,11 +40,9 @@ BOOST_AUTO_TEST_CASE(PassTest) {
     outfile << encryption(rawfile, password).rdbuf();
     outfile.close();
 
-
     std::ifstream infile(filename, std::ios::binary);
 
     std::string result = decryption(infile, password).str();
-
-    BOOST_CHECK_EQUAL(teststr, result);
     infile.close();
+    BOOST_CHECK_EQUAL(teststr, result);
 }
